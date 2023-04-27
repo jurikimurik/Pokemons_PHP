95f2b8931427370e1463987eb6f32724453f264f
s26969 <makovskyiyurii@gmail.com>
2023-04-27 17:57:46 +0200

    Base visual implemented.

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
+require "visualScript.php";
+
+
+#include "Pokemons/FirePokemon.php";
+
+#$Charizard = new \Pokemons\FirePokemon("Charizard",100);
+
+require "PokemonMain/Pokemon.php";
+
+$pokemon = new Pokemon("Charizard", 100, [0,0,0]);
+?>
+
+<html>
+<style>
+    table, th, td {
+        border:1px solid black;
+    }
+</style>
+<body>
+
+<?php
+echo narysujPokemona($pokemon);
+?>
+
+
+
+
+
 
---
+++ visualScript.php
@@ -0,0 +1,59 @@
+<?php
+
+
+function narysujPokemonaZTablicy($tablica) {
+    $str = "<table>
+    <tr>
+        <th>Nazwa</th>
+        <th>Zywiol</th>
+        <th>Liczba HP</th>
+        <th>Aktualna Liczba HP</th>
+    </tr>
+    <tr>
+        <td>"."$tablica[0]"."</td>
+        <td>"."$tablica[1]"."</td>
+        <td>"."$tablica[2]"."</td>
+        <td>"."$tablica[3]"."</td>
+</tr>
+</table>";
+
+
+
+    return <<<HTML
+    <html>
+    <body><h1>{$str}</h1>
+    </body>
+    </html>
+HTML;
+}
+
+
+function narysujPokemona(Pokemon $pokemon) {
+    $name = $pokemon->getName();
+    $type = $pokemon->getPokemonType();
+    $maxHp = $pokemon->getMaxHp();
+    $hp = $pokemon->getCurrentHp();
+    $str = "<table>
+    <tr>
+        <th>Nazwa</th>
+        <th>Zywiol</th>
+        <th>Liczba HP</th>
+        <th>Aktualna Liczba HP</th>
+    </tr>
+    <tr>
+        <td>"."$name"."</td>
+        <td>"."JAKIS"."</td>
+        <td>"."$maxHp"."</td>
+        <td>"."$hp"."</td>
+</tr>
+</table>";
+
+
+
+    return <<<HTML
+    <html>
+    <body><h1>{$str}</h1>
+    </body>
+    </html>
+HTML;
+}
