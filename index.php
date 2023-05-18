17633553ba04928f64aa947757ce4f0ccfa4bc1e
Yurii <makovskyiyurii@gmail.com>
2023-05-18 11:00:53 +0200

    Form for pokemon choose and submit button.

--- index.php
+++ index.php
@@ -4,13 +4,10 @@ require "visualScript.php";
 
 include "Pokemons/FirePokemon.php";
 include "Pokemons/GrassPokemon.php";
+include "Pokemons/WaterPokemon.php";
 
-$Charizard = new \Pokemons\FirePokemon("Charizard",100);
-$Trawnik = new \Pokemons\GrassPokemon("Trawnik", 100);
+include "fight.php";
 
-#require "PokemonMain/Pokemon.php";
-
-#$pokemon = new Pokemon("Charizard", 100, [0,0,0]);
 ?>
 
 <html>
@@ -19,11 +16,41 @@ $Trawnik = new \Pokemons\GrassPokemon("Trawnik", 100);
         border:1px solid black;
     }
 </style>
+<FORM action="fight.php" method = "POST">
+    <table>
+        <TR>
+            <TD>
+                <label for="pokemon1">Pokemon 1:</label>
+                <select name="pokemon1" id="pokemon1">
+                    <option value="">--- Wybierz ---</option>
+                    <option value="fire">Ogien</option>
+                    <option value="grass">Trawa</option>
+                    <option value="water">Woda</option>
+                </select>
+            </TD>
+        </TR>
+        <TR>
+            <TD>
+                <label for="pokemon2">Pokemon 2:</label>
+                <select name="pokemon2" id="pokemon2">
+                    <option value="">--- Wybierz ---</option>
+                    <option value="fire">Ogien</option>
+                    <option value="grass">Trawa</option>
+                    <option value="water">Woda</option>
+                </select>
+            </TD>
+        </TR>
+        <TR>
+            <TD>
+                <INPUT TYPE = SUBMIT>
+            </TD>
+        </TR>
+    </table>
+</FORM>
 <body>
 
 <?php
-echo narysujPokemona($Charizard);
-echo narysujPokemona($Trawnik);
+
 ?>
 
 
